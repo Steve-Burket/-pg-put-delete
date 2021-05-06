c3f5cbf49a2d632c43a8b0f5d9d7121e380e531b
Steve Burket <burketsteve@gmail.com>
2021-05-06 15:35:45 -0500

    delete function is working; started up updateBook function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,80 +1,100 @@
-$(document).ready(function(){
-  console.log('jQuery sourced.');
+$(document).ready(function () {
+  console.log("jQuery sourced.");
   refreshBooks();
   addClickHandlers();
 });
 
 function addClickHandlers() {
-  $('#submitBtn').on('click', handleSubmit);
+  $("#submitBtn").on("click", handleSubmit);
 
   // TODO - Add code for edit & delete buttons
-  $('#delete').on('click', deleteBook);
-  // $('#updateBook').on('click', updateBook);
+  $("#bookShelf").on("click", ".delete", deleteBook);
+  $('#bookShelf').on('click', ".isRead", updateBook);
+}
+
+function updateBook(isRead, bookid) {
+  
+  $.ajax({
+    method: "PUT",
+    url: `/books/${bookid}/isRead`,
+    data: isRead
+  }).then(function(response) {
+    $('.isRead').text("Read it");
+    refreshBooks();
+  }).catch(function(error) {
+    console.log('Error marking as read', error);
+    
+  })
 }
 
 function deleteBook(event) {
   console.log(event.target);
   const bookid = $(event.target).data("bookid");
-
-  console.log('deleting a bok with id of: ', bookid);
+  console.log("deleting a book with id of: ", bookid);
   $.ajax({
     url: `/books/${bookid}`,
-    method: 'DELETE'
-  }).then(function(serverResponse) {
-    renderBooks();
-  }).catch(function (error) {
-    alert('Error in deleting book', error);
+    method: "DELETE",
   })
+    .then(function (serverResponse) {
+      refreshBooks();
+    })
+    .catch(function (error) {
+      console.log("Error in deleting book", error);
+    });
 }
 
 function handleSubmit() {
-  console.log('Submit button clicked.');
+  console.log("Submit button clicked.");
   let book = {};
-  book.author = $('#author').val();
-  book.title = $('#title').val();
+  book.author = $("#author").val();
+  book.title = $("#title").val();
   addBook(book);
 }
 
 // adds a book to the database
 function addBook(bookToAdd) {
   $.ajax({
-    type: 'POST',
-    url: '/books',
+    type: "POST",
+    url: "/books",
     data: bookToAdd,
-    }).then(function(response) {
-      console.log('Response from server.', response);
+  })
+    .then(function (response) {
+      console.log("Response from server.", response);
       refreshBooks();
-    }).catch(function(error) {
-      console.log('Error in POST', error)
-      alert('Unable to add book at this time. Please try again later.');
+    })
+    .catch(function (error) {
+      console.log("Error in POST", error);
+      alert("Unable to add book at this time. Please try again later.");
     });
 }
 
 // refreshBooks will get all books from the server and render to page
 function refreshBooks() {
   $.ajax({
-    type: 'GET',
-    url: '/books'
-  }).then(function(response) {
-    console.log(response);
-    renderBooks(response);
-  }).catch(function(error){
-    console.log('error in GET', error);
-  });
+    type: "GET",
+    url: "/books",
+  })
+    .then(function (response) {
+      console.log(response);
+      renderBooks(response);
+    })
+    .catch(function (error) {
+      console.log("error in GET", error);
+    });
 }
-
 
 // Displays an array of books to the DOM
 function renderBooks(books) {
-  $('#bookShelf').empty();
-
-  for(let i = 0; i < books.length; i += 1) {
+  $("#bookShelf").empty();
+  for (let i = 0; i < books.length; i += 1) {
     let book = books[i];
     // For each book, append a new row to our table
-    $('#bookShelf').append(`
+    $("#bookShelf").append(`
       <tr>
+        <td><button data-bookid=${book.id} class="isRead">Mark as read</button></td>
         <td>${book.title}</td>
         <td>${book.author}</td>
+        <td><button class="delete" data-bookid=${book.id}>Delete</button></td>
       </tr>
     `);
   }
--- server/routes/book.router.js
+++ server/routes/book.router.js
@@ -1,34 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const pool = require('../modules/pool');
+const pool = require("../modules/pool");
 
 // Get all books
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   let queryText = 'SELECT * FROM "books" ORDER BY "title";';
-  pool.query(queryText).then(result => {
-    // Sends back the results in an object
-    res.send(result.rows);
-  })
-  .catch(error => {
-    console.log('error getting books', error);
-    res.sendStatus(500);
-  });
+  pool
+    .query(queryText)
+    .then((result) => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log("error getting books", error);
+      res.sendStatus(500);
+    });
 });
 
 // Adds a new book to the list of awesome reads
 // Request body must be a book object with a title and author.
-router.post('/',  (req, res) => {
+router.post("/", (req, res) => {
   let newBook = req.body;
   console.log(`Adding book`, newBook);
 
   let queryText = `INSERT INTO "books" ("author", "title")
                    VALUES ($1, $2);`;
-  pool.query(queryText, [newBook.author, newBook.title])
-    .then(result => {
+  pool
+    .query(queryText, [newBook.author, newBook.title])
+    .then((result) => {
       res.sendStatus(201);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(`Error adding new book`, error);
       res.sendStatus(500);
     });
@@ -38,11 +41,48 @@ router.post('/',  (req, res) => {
 // Updates a book to show that it has been read
 // Request must include a parameter indicating what book to update - the id
 // Request body must include the content to update - the status
+router.put("/:bookid/isRead", (req, res) => {
+  const bookid = req.params.bookid;
+  const isRead = req.body.isRead;
+  let queryText = "";
 
+  if (isRead == true) {
+    queryText = 'UPDATE "books" SET "isRead"="true" WHERE id=$1 RETURNING *;';
+    res.status(201).send("Book is marked as read");
+  }
 
-// TODO - DELETE 
+  console.log(bookid, isRead);
+  pool
+    .query(queryText, [bookid])
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log(`Error making query: ${queryText}`, err);
+      res.sendStatus(500);
+    });
+});
+
+// TODO - DELETE
 // Removes a book to show that it has been read
 // Request must include a parameter indicating what book to update - the id
+router.delete("/:bookid", (req, res) => {
+  console.log(req.body);
+  console.log(req.params);
+  const idToGet = req.params.bookid;
+  console.log(idToGet);
 
+  const queryText = 'DELETE FROM "books" WHERE id=$1';
+  pool
+    .query(queryText, [idToGet]) // why does the idToGet variable need to be in brackets?[]
+    .then((result) => {
+      console.log(`Deleted book with id ${idToGet}`);
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log(`Error making query: ${queryText}`, err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
